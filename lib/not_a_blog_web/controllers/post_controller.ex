361cae03811eead8fc28db3469cbc29c9e9575d0
Alexander Teslovskiy <artoriousso@gmail.com>
2019-08-08 13:34:12 +0300

    Add post edit page

--- lib/not_a_blog_web/controllers/post_controller.ex
+++ lib/not_a_blog_web/controllers/post_controller.ex
@@ -15,21 +15,34 @@ defmodule NotABlogWeb.PostController do
   end
 
   def new(conn, _params) do
-    render(conn, "new.html", changeset: Post.changeset(%Post{}))
+    render(conn, "new.html", changeset: empty_post)
   end
 
-  def edit(conn, _params) do
-    render(conn, "edit.html")
+  def edit(conn, %{"id" => id} = _params) do
+    post = Repo.get(Post, id)
+    render(conn, "edit.html", post: post, changeset: Post.changeset(post, %{}))
   end
 
-  def create(conn, %{"post" => %{"title" => title, "content" => content}} = _params) do
-    changeset = Post.changeset(%Post{}, %{title: title, content: content})
+  def create(conn, %{"post" => post_params} = _params) do
+    changeset = Post.changeset(%Post{}, post_params)
     case Repo.insert(changeset) do
       {:ok, post} ->
         conn
         |> put_flash(:info, "Post successfully created")
         |> redirect(to: post_path(conn, :show, post.id))
       {:error, changeset} -> render conn, "new.html", changeset: changeset
+    end
+  end
+
+  def update(conn, %{"id" => id, "post" => post_params} = _params) do
+    post = Repo.get!(Post, id)
+    changeset = Post.changeset(post, post_params)
+    case Repo.update(changeset) do
+      {:ok, post} ->
+        conn
+        |> put_flash(:info, "Post successfully updated")
+        |> redirect(to: post_path(conn, :show, post.id))
+      {:error, changeset} -> render conn, "edit.html", post: post, changeset: changeset
     end
   end
 
@@ -45,5 +58,9 @@ defmodule NotABlogWeb.PostController do
         |> put_flash(:error, "Something went wrong")
         |> redirect to: post_path(conn, :index)
     end
+  end
+
+  defp empty_post do
+    Post.changeset(%Post{}, %{})
   end
 end
