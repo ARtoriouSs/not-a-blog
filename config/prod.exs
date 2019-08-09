a8c0f069d428fe2b1f144a2e5e18c80b063d4abb
Alexander Teslovskiy <artoriousso@gmail.com>
2019-08-09 13:51:38 +0300

    Add heroku configuration

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,8 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :not_a_blog, NotABlogWeb.Endpoint,
-  url: [host: "example.com", port: 80],
+  url: [scheme: "https", host: "radiant-depths-24465", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
